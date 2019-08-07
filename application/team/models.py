3f546e78c7ea492d9b98d36baea88b0f6f961b71
Toivo Kärkinen <toivo.karkinen@helsinki.fi>
2019-08-07 20:25:36 +0300

    team models ref fix

--- application/team/models.py
+++ application/team/models.py
@@ -5,6 +5,8 @@ class Team(db.Model):
    shortname = db.Column(db.String(3))
    longname = db.Column(db.String(15))
 
+   user = db.relationship("User", backref='account', lazy=True)
+
    def __init__(self, shortname, longname):
      self.shortname = shortname
      self.longname = longname
