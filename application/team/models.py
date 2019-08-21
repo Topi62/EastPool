2d485b3f7fb36526c86ec1b02f405e9f332c0433
Toivo Kärkinen <toivo.karkinen@helsinki.fi>
2019-08-21 18:57:48 +0300

    sqlite vs postgre try to fix 1

--- application/team/models.py
+++ application/team/models.py
@@ -38,7 +38,7 @@ class Team(db.Model):
 		 " (SELECT COALESCE(sum(visitorframewins),0) FROM game JOIN match ON game.idmatch = match.idmatch WHERE idseason= :season AND hometeamid = team.shortname  ) +"
 		 " (SELECT COALESCE(sum(homeframewins),0) FROM game JOIN match ON game.idmatch = match.idmatch WHERE idseason= :season AND visitorteamid = team.shortname ) AS erätappiot"     
 		 " FROM team "
-	         " GROUP BY team.longname ORDER BY voitot DESC, (pelivoitot - pelitappiot) DESC, (erävoitot - erätappiot) DESC ").params(season = seasonid)	 
+	         " GROUP BY team.longname ORDER BY voitot DESC, pelivoitot - pelitappiot DESC, erävoitot - erätappiot DESC ").params(season = seasonid)	 
 		
      res = db.engine.execute(stmt)
      return res 
